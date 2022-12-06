51abaffedd92fbf68b4a6b266f54e4b9e6b1d29e
teki45 <goteki@protonmail.com>
2022-12-06 13:10:34 -0800

    Transfer files from previous project to new one

---
+++ omnipotent/resources/views/components/card.blade.php
@@ -0,0 +1,4 @@
+<div class="card">
+    <div id="cd-title">{{ $title }}</div>
+    <div id="cd-content">{{ $slot }}</div>
+</div>
---
+++ omnipotent/resources/views/components/content.blade.php
@@ -0,0 +1,10 @@
+<div id="content">
+    <div id="left-side">
+        <x-card title="Card 1">
+            Test content
+        </x-card>
+    </div>
+    <div id="right-side">
+        {{ $slot }}
+    </div>
+</div>
